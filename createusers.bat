babab17af4f55473cc1bafd11f1ad4d1ac60c2c0
mpeter92 <mpeter92@yahoo.co.uk>
2017-10-15 21:48:41 +0100

    Add files via upload

---
+++ createusers.bat
@@ -0,0 +1,21 @@
+::create csv file and set the object names 
+
+Echo "DN,objectClass,sAMAccountName,sn,givenName,userPrincipalName" > c:\temp\users.csv
+
+
+::set the domain name
+Set /p _domain="enter you domain name here, leave out the .com, .net part:  "
+Set /p _domainroot="enter the root domain, e.g. com, net:  "
+
+
+::Take the user name from users.txt and compile into the csv file for importing.
+for /f "tokens=1,2 delims= " %%G in (c:\temp\users.txt) do echo "CN=%%G %%H,OU=users,DC=%_domain%,DC=%_domainroot%",user,%%G%%H,%%H,%%G,%%G.%%H@%_domain%.%_domainroot% >> c:\temp\users.csv
+
+
+::import users to active directory
+
+csvde -i c:\temp\users.csv
+
+
+::
+::for /f "tokens=*" %%G IN ('dsquery user "OU=users,DC=%_domain%,DC=%_domainroot%"') do dsmod user %%G -pwd password -disabled No
